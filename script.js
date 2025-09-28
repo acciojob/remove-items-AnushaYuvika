a0ee40974a662e9f4ae26d2f5cc8d787201f1c37
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-28 21:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let inp = document.querySelector("input");
+let select = document.querySelector("#colorSelect");
+
+inp.addEventListener("click", function() {
+	let selectedIndex = select.selectedIndex;
+	if (selectedIndex !== -1) {
+		select.remove(selectedIndex);
+	}
+});
